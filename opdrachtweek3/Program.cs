1c0d61277f86395e4f4cf61f97b86bb1686441d3
Thor Boonaert <thor.boonaert@student.arteveldehs.be>
2017-10-23 15:59:43 +0200

    commit

---
+++ opdrachtweek3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1718_csse_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
