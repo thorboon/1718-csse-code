9cb17e9c403fe48402e6adbedd645790c0dbf5bc
Thor Boonaert <thor.boonaert@student.arteveldehs.be>
2018-01-12 12:17:12 +0100

    aanvulling

--- opdrachtweek03/Program.cs
+++ opdrachtweek03/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+
 
 namespace opdrachtweek03
 {
@@ -6,37 +10,56 @@ namespace opdrachtweek03
     {
         static void Main(string[] args)
         {
-            // Oplossing 1: 
-            Console.WriteLine("Van welk getal wil je vermenigvuldigingstafel?");
-            int getal = Convert.ToInt32(Console.ReadLine());
-            int[] resultaten = new int[11];
-            for (int i = 0 ; i < 11 ; i++)
-            {
-                int resultaat = i * getal;
-                Console.WriteLine("{0} x {1} = {2}", i, getal, resultaat);
-                resultaten[i] = resultaat;
-            }
-
-            Console.WriteLine("Resultaten overlopen, eerste loop...");
-            foreach (var item in resultaten)
-            {
-                Console.WriteLine("{0}", item);
-            }
-            Console.WriteLine("Resultaten overlopen, tweede loop...");
-            for (int i = 0; i < resultaten.Length; i++)
-            {
-                Console.WriteLine("{0}", resultaten[i]);
-            }
-
-
-            //vraagGetal()
-            // datacollectie 1
-            // datacollectie 2
-            // datacollectie 3
-            //maakVermenigvuldigingstafel(getal)
-            //
-
             
+            //Ask for a number
+            Console.WriteLine("Give in a number:");
+            int getal = Convert.ToInt32((Console.ReadLine()));
+            // writing
+            Console.WriteLine("Vermenigvuldigingsfacotr: {0}", getal);
+            //create array with 11 places
+            int[] a = new int[11];
+           
+           
+            //create new list
+             List <int> lijst = new List <int>();
+
+            //create new hashtable
+            Hashtable tafels = new Hashtable();
+                    
+
+            //loop
+            for (int i = 0; i < 11; i++){
+                
+                a[i]= getal*i;
+            
+	            lijst.Add(i*getal);
+
+  
+               tafels.Add(i,i*getal);
+	
+            }
+            Console.WriteLine("Array");
+            //write more stuff
+            int teller = 0;
+            foreach(int j in a){
+                Console.WriteLine("{0}x{1}={2}",teller, getal, j);
+                teller++;
+            }
+
+            Console.WriteLine("List");
+            //write a list
+            teller = 0;
+            foreach(int j in lijst){
+                Console.WriteLine("{0}x{1}={2}",teller, getal, j);
+                teller++;
+            }
+
+            Console.WriteLine("Hashtable");
+            //more hashtables
+            for(int i=0; i<11; i++){
+                Console.WriteLine("{0}x{1}={2}",i, getal, tafels[i]);
+            }
+          
         }
     }
 }
