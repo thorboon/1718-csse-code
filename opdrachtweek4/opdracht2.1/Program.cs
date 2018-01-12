8b67b68d392c5f6ed0b3ccf0a25d4ea759bb2837
Thor Boonaert <thor.boonaert@student.arteveldehs.be>
2018-01-12 12:25:13 +0100

    aanvulling

---
+++ opdrachtweek4/opdracht2.1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace opdracht2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = GeefGetal();
+            if (number < 100)
+            {
+                for (int i = 0; i < (number+1); i++)
+                {
+                    Console.WriteLine(i + IsPrime(i));
+                }
+            } 
+            else
+            {
+                Console.WriteLine("Het getal is groter dan 100, foei!");
+            }
+
+        }
+
+
+        static int GeefGetal()
+        {
+            Console.WriteLine("Geef een geheel getal in (max tot 100)");
+            int getal = Convert.ToInt32(Console.ReadLine());
+            return getal;
+        }
+
+        static string IsPrime(int getal)
+        {
+            int tllr = 0;
+            for (int i = 1; i < (getal+1); i++)
+            {
+                if((getal%i) == 0) 
+                {
+                  
+                    tllr++;
+                }
+            }
+            
+            if(tllr == 2)
+            {
+                return " is een priemgetal";
+            }
+            else
+            {
+                return " is geen priemgetal";
+            }
+        }
+    }
+
+}
---
+++ opdrachtweek4/opdracht2.2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace opdracht2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = GeefGetal();
+            Fibo(number);
+
+
+        }
+
+        static int GeefGetal()
+        {
+            Console.WriteLine("Geef een geheel getal in: ");
+            int getal = Convert.ToInt32(Console.ReadLine());
+            return getal;
+
+        }
+
+        static void Fibo(int number)
+        {
+            int[] a = new int[number];
+            a[0] = 0;
+            a[1] = 1;
+            for (int i = 2; i < a.Length ; i++)
+            {
+                a[i]= a[i-1] + a[i-2];
+            }
+            int teller = 0;
+
+            foreach(var j in a)
+            {
+                Console.WriteLine(a[teller]);
+                teller++;
+            }
+
+        }
+
+    }
+}
